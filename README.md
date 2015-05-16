8a69852a8602b6a0491a00a6bbffc67ba609f699
Vincent Shieh <vincent.shieh@gmail.com>
2015-05-16 14:42:49 -0700

    merge two recursion files

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
 # Ruby Recursion
 
-Some recursion problems solved in Ruby.
+Some recursion problems solved in Ruby:
 
-## recursion.rb
 * Given a start and end integer, return an array of all of the integers in between the two
 * Return the sum of an array
 * Two implementations of exponentiation
 * ```Array#deep_dup```
 * Fibonacci numbers
 * Binary search
-
-## recursion2.rb
 * Merge sort
 * Return all subsets of an array
 
--- recursion.rb
+++ recursion.rb
@@ -93,3 +93,41 @@ def binary_search(arr, target)
     current_position + 1 + result
   end
 end
+
+def merge_sort(arr)
+  if arr.length == 1
+    return arr
+  else
+    merge(merge_sort(arr[0..arr.length/2-1]), merge_sort(arr[arr.length/2..-1]))
+  end
+end
+
+def merge(arr1, arr2)
+  merged_arr = []
+
+  until arr1.empty? || arr2.empty?
+    if arr1[0] < arr2[0]
+      merged_arr << arr1.shift
+    else
+      merged_arr << arr2.shift
+    end
+  end
+
+  merged_arr += arr1 if arr2.empty?
+  merged_arr += arr2 if arr1.empty?
+
+  merged_arr
+end
+
+def subsets(arr)
+  if arr.length == 0
+    [[]]
+  elsif arr.length == 1
+    [[], [arr[0]]]
+  else
+    previous = subsets(arr[0..-2])
+    mapped = deep_dup(previous)
+    mapped.each { |el| el << arr.last }
+    previous + mapped
+  end
+end
